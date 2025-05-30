6bc67327c3f895eb00cb4cb7547f07e3b039b6b8
CaoTrongHai <tronghaipdp@gmail.com>
2025-05-30 14:16:05 +0700

    feat zone v2

---
+++ src/modules/zone/zone.schema.js
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const createZone = z
+  .object({
+    name: z.string().min(1, "Zone name is required"),
+    totalCapacity: z
+      .number({
+        required_error: "Total capacity is required",
+      })
+      .min(1, "Total capacity must be greater than 0"),
+    storageTemperature: z
+      .object({
+        min: z.number({
+          required_error: "Minimum storage temperature is required",
+        }),
+        max: z.number({
+          required_error: "Maximum storage temperature is required",
+        }),
+      })
+      .refine((data) => data.min < data.max, {
+        message:
+          "Minimum storage temperature must be less than maximum storage temperature",
+      }),
+  })
+  .strict();
